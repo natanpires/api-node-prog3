0b3ad2c98e58fea28322c46e6ca8d60995c0a034
Natan Souza <partisansouza@gmail.com>
2021-04-14 14:58:48 -0300

    refactor(aula.js): adopt early return and adjust some errors.

--- src/controllers/aula.js
+++ src/controllers/aula.js
@@ -7,11 +7,9 @@ function Aulas() {
     const id = parseInt(params.id) || false;
     try {
       if (!id) {
-        const aulas = await this.AulaModel.findAll();
-        return aulas;
+        return await this.AulaModel.findAll();
       }
-      const aula = await this.AulaModel.findByPk(id);
-      return aula;
+      return await this.AulaModel.findByPk(id);
     } catch (error) {
       throw { error, code: 422 };
     }
@@ -19,8 +17,7 @@ function Aulas() {
 
   this.create = async ({ body }) => {
     try {
-      const aula = await this.AulaModel.create(body);
-      return aula;
+      return await this.AulaModel.create(body);
     } catch (error) {
       throw { error, code: 422 };
     }
@@ -29,12 +26,13 @@ function Aulas() {
   this.update = async ({ body, params }) => {
     const id = parseInt(params.id);
     try {
+      if (!body.name) throw { message: "MISSING PARAMETER", parameter: "name", code: 400 }
       const aula = await this.AulaModel.findByPk(id);
       aula.nome = body.nome;
       await aula.save();
       return aula;
     } catch (error) {
-      throw { error, code: 422 };
+      throw { error, code: error?.code || 422 };
     }
   };
 
